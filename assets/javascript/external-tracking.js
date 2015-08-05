63d6114f24ab081060aa7c1ade504585ef01b0e2
leandrooriente <leandroriente@gmail.com>
2015-08-05 11:39:51 -0300

    Track selected text

--- assets/javascript/external-tracking.js
+++ assets/javascript/external-tracking.js
@@ -1,15 +1,31 @@
-var trackOutboundLink = function(url) {
+function getSelected() {
+  var text = "";
+  if (window.getSelection) {
+      text = window.getSelection().toString();
+  } else if (document.selection && document.selection.type != "Control") {
+      text = document.selection.createRange().text;
+  }
+  return text;
 }
 
+
 $(document).ready(function(){
-    $('.post-content').on('click', '.uber-external-link', function(e) {
-      var _url = $(this).attr('href');
+  var $post = $('.post-content');
 
-      ga('send', 'event', 'uber', 'click', _url, {'hitCallback':
-        function () { document.location = _url; }
-      });
+  $post.on('click', '.uber-external-link', function(e) {
+    var _url = $(this).attr('href');
 
-      e.preventDefault();
+    ga('send', 'event', 'uber', 'click', _url, {'hitCallback':
+      function () { document.location = _url; }
     });
 
+    e.preventDefault();
+  });
+
+  $post.on('copy', function(e) {
+    var _text = getSelected();
+    if (_text && _text.indexOf('uber20pratas') !== -1) {
+      ga('send', 'event', 'uber', 'copy', _text);
+    }
+  });
 });
