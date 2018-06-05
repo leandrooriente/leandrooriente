b31705355d8a8daac7b73acb2428fe2fb0755154
leandrooriente <leandroriente@gmail.com>
2018-06-05 08:49:06 +0200

    Add coverage support

--- test/jest.config.js
+++ test/jest.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   rootDir: '../',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '!**/index.js',
+    '!**/propTypes/**/*.js',
+    '**/components/**/*.js',
+    '**/helpers/**/*.js',
+  ],
   setupFiles: ['<rootDir>/test/jest.setup.js'],
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   moduleNameMapper: {
