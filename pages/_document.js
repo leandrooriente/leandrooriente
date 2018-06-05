d451313f852a85eef8148a06bac050e3abad8f81
leandrooriente <leandroriente@gmail.com>
2018-06-05 08:48:50 +0200

    Fix production build

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Document, { Head, Main, NextScript } from 'next/document';
 import 'normalize.css';
-import 'styles/theme.css';
 
 export default class MyDocument extends Document {
   render() {
@@ -9,6 +8,8 @@ export default class MyDocument extends Document {
       <html lang="en">
         <Head>
           <link rel="stylesheet" href="/_next/static/style.css" />
+          <link rel="stylesheet" href="/static/normalize.css" />
+          <link rel="stylesheet" href="/static/theme.css" />
           <link href="https://fonts.googleapis.com/css?family=Lora|Source+Sans+Pro:400,700" rel="stylesheet" />
           <meta name="viewport" content="width=device-width,initial-scale=1" />
         </Head>
